0700da394b4116ee513ce695982060ba523f46a5
Hidayatus Sholikhin <sholikhinhidayatus@gmail.com>
2025-08-29 19:37:55 +0700

    fix: cosmetics

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,26 +5,13 @@
 @section('content')
     <div class="account-pages my-5 pt-sm-5">
         <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="text-center">
-                        <a href="{{ url('index') }}" class="mb-5 d-block auth-logo">
-                            <img src="{{ URL::asset('/assets/images/logo-dark.png') }}" alt="" height="22"
-                                class="logo logo-dark">
-                            <img src="{{ URL::asset('/assets/images/logo-light.png') }}" alt="" height="22"
-                                class="logo logo-light">
-                        </a>
-                    </div>
-                </div>
-            </div>
             <div class="row align-items-center justify-content-center">
                 <div class="col-md-8 col-lg-6 col-xl-5">
                     <div class="card">
 
                         <div class="card-body p-4">
                             <div class="text-center mt-2">
-                                <h5 class="text-primary">Welcome Back !</h5>
-                                <p class="text-muted">Sign in to continue to Minible.</p>
+                                <img src="{{ URL::asset('/assets/images/yunas_dark.png') }}" alt="" height="64">
                             </div>
                             <div class="p-2 mt-4">
                                 <form method="POST" action="{{ route('login') }}">
@@ -33,7 +20,7 @@
                                     <div class="mb-3">
                                         <label class="form-label" for="username">Username</label>
                                         <input type="text" class="form-control @error('username') is-invalid @enderror"
-                                            name="username" value="{{ old('username', 'sayunas') }}" id="username"
+                                            name="username" value="{{ old('username') }}" id="username"
                                             placeholder="Enter Username">
                                         @error('username')
                                             <span class="invalid-feedback" role="alert">
@@ -43,26 +30,14 @@
                                     </div>
 
                                     <div class="mb-3">
-                                        <div class="float-end">
-                                            @if (Route::has('password.request'))
-                                                <a href="{{ route('password.request') }}" class="text-muted">Forgot
-                                                    password?</a>
-                                            @endif
-                                        </div>
                                         <label class="form-label" for="userpassword">Password</label>
                                         <input type="password" class="form-control @error('password') is-invalid @enderror"
-                                            value="sayunas" name="password" id="userpassword" placeholder="Enter password">
+                                            value="{{ old('password') }}" name="password" id="userpassword" placeholder="Enter password">
                                         @error('password')
                                             <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $message }}</strong>
                                             </span>
                                         @enderror
-                                    </div>
-
-                                    <div class="form-check">
-                                        <input type="checkbox" class="form-check-input" id="auth-remember-check"
-                                            name="remember" {{ old('remember') ? 'checked' : '' }}>
-                                        <label class="form-check-label" for="auth-remember-check">Remember me</label>
                                     </div>
 
                                     <div class="mt-3 text-end">
@@ -79,7 +54,7 @@
                         <p>© <script>
                                 document.write(new Date().getFullYear())
 
-                            </script> Minible. Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesbrand</p>
+                            </script> Yunas Studio.</p>
                     </div>
 
                 </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -183,28 +183,6 @@
         </div>
     </div>
 </div>
-
-<div class="row">
-    <div class="col-xl-8">
-        <div class="card">
-            <div class="card-body">
-                <div class="float-end">
-                    <div class="dropdown">
-                        <a class="dropdown-toggle text-reset" href="#" id="dropdownMenuButton5" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="fw-semibold">Sort By:</span> <span class="text-muted">Yearly<i class="mdi mdi-chevron-down ms-1"></i></span>
-                        </a>
-
-                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton5">
-                            <a class="dropdown-item" href="#">Monthly</a>
-                            <a class="dropdown-item" href="#">Yearly</a>
-                            <a class="dropdown-item" href="#">Weekly</a>
-                        </div>
-                    </div>
-                </div>
-            </div> 
-        </div> 
-    </div> 
-</div> <!-- end row-->
 <!-- end row -->
 
 @endsection
