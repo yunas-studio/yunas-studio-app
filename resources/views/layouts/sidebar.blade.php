397d30f3d713740dd72f099e7e161cdba6ef00a3
Hidayatus Sholikhin <sholikhinhidayatus@gmail.com>
2025-09-18 23:49:31 +0700

    fix: route index to transaction when role as a user

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,7 +3,7 @@
 
     <!-- LOGO -->
     <div class="navbar-brand-box">
-        <a href="{{url('index')}}" class="logo logo-dark">
+        <a href="{{Auth::user()->isUser() ? url('transaksi') : url('index')}}" class="logo logo-dark">
             <span class="logo-sm">
                 <img src="{{ URL::asset('/assets/images/yunas_dark.png') }}" alt="" height="12">
             </span>
